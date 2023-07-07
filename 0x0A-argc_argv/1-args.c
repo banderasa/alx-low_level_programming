f11c3353c0d717c18ef580a2662dc895088b7935
David <davidjeseigweh.com>
2023-07-07 13:07:48 -0700

    made some modifications

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * main - prints the number of argumentsv
- * @argc: number of arguments 
+ * @argc: number of arguments
  * @argv: arrays in the argument
  * Return: Always 0 (Success)
  */
@@ -10,7 +10,7 @@ int main(int argc __attribute__((unused)), char *argv[])
 {
 	int argnumber = argc - 1;
 	(void) argv; /*Ignore argv*/
-	
+
 	printf("%d\n", argnumber);
-	return (0) ;
+	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include "main.h"
-/**main - A program that prints all arguments it recieves
- * @argc: number of arguments 
+/**
+ * main - A program that prints all arguments it recieves
+ * @argc: number of arguments
  * @argv: arrays in an argument
  * Return:  always 0
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
-		
-		return 0;
+		return (0);
 }
